da8df2ffad886c84bef291944a01b26731bd49e8
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2025-06-01 21:24:18 +0000

    Atualizações atv 3

--- src/componentes/Cabecalho.tsx
+++ src/componentes/Cabecalho.tsx
@@ -1,18 +1,19 @@
 const Titulo = () => (
-	<h1 className="text-2xl font-bold mb-1">React - Conceitos básicos</h1>
+  <h1 className="text-2xl md:text-3xl font-bold mb-1">React - Conceitos básicos</h1>
 );
 
 const SubTitulo = () => (
-	<h2 className="text-4xl font-bold mb-6">Lista de tarefas</h2>
+  <h2 className="text-3xl md:text-4xl font-bold mb-6">Lista de tarefas</h2>
+  
 );
 
 const Cabecalho = () => {
-	return (
-		<div className="text-center">
-			<Titulo />
-			<SubTitulo />
-		</div>
-	);
+  return (
+    <div className="text-center my-6">
+      <Titulo />
+      <SubTitulo />
+    </div>
+  );
 };
 
 export default Cabecalho;
